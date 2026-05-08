27f780afe481deec74512274ae1564f99b61dc8a
Dhanu9797 <pathadedhanashri43@gmail.com>
2026-05-08 16:22:28 +0530

    fixed the port number and version issue

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:8081',
-  keycloakUrl: 'http://localhost:8082',
+  keycloakUrl: 'http://192.168.10.136:8082',
   keycloakRealm: 'ceo-dashboard',
   keycloakClientId: 'ceo-backend'
 };

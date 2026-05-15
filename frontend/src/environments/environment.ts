55785f4747732fdaf400851fdcd158aa4e69d7fd
wandharerajnandini-ctrl <wandharerajnandini@gmail.com>
2026-05-15 11:07:33 +0530

    Remove environment.ts from tracking

--- frontend/src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8081',
-  // Keycloak base URL (without /realms/...)
-  keycloakUrl: 'http://localhost:8080',
-  keycloakRealm: 'ceo-dashboard',
-  // Use the client ID you provided (underscore)
-  keycloakClientId: 'ceo_backend'
-};

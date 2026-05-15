674bfd44a298ed40325c93220fec2403c5549b2d
Shradha Pawar <162123840+pawar186@users.noreply.github.com>
2026-05-15 11:10:25 +0530

    Stop tracking environment.ts

--- frontend/src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8081',
-  keycloakUrl: 'http://localhost:8082',
-  keycloakRealm: 'ceo-dashboard',
-  keycloakClientId: 'ceo-backend'
-};
